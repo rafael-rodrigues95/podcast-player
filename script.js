fc63b056556d48b4adce8c326ee778ab44a70ad6
rafael r souza <fael_rs@live.com>
2023-09-19 10:58:50 -0300

    Edição dos comentários

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Nota: Scrubbing é o evento de se vasculhar uma timeline enquanto seguramos o cursos do mouse sobre ela
 
 let musicContainer = document.querySelector(".music-container");
 let timelineContainer = document.querySelector(".timeline-container");
@@ -14,14 +14,22 @@ timelineContainer.style.setProperty("--background", "black")
 
 const music_list = [
   {
-    img: "./_images/cd.jpg",
+    id: "1",
+    img: "./_images/cd1.jpg",
+    name: "Swan Lake - Act IV Finale",
+    artist: "Tchaikovsky",
+    music: "./_music/swan-lake.mp3",
+  },
+  {
+    id: "2",
+    img: "./_images/cd2.jpg",
     name: "Swan Lake - Act IV Finale",
     artist: "Tchaikovsky",
     music: "./_music/swan-lake.mp3",
   },
 ]
 
-//Play/Pause button toggle
+//Play/Pause interruptor
 playPauseBtn.addEventListener("click", togglePlay)
 
 /*Play/Pause*/
@@ -38,7 +46,7 @@ function togglePlay() {
   }
 }
 
-//Add paused class on paused and remove on play
+// Adiciona pausado na classe e o remove quando o audio está em play
 audio.addEventListener("play", () => {
   musicContainer.classList.remove("paused")
 })
@@ -52,20 +60,20 @@ function reset() {
   totalTime.textContent = "00:00";
 }
 
-//Timeline
+// Timeline
 
-//If mouse is moving starts handleTimelineUpdate
+// Se o mouse está se movendo, starta o handleTimelineUpdate
 timelineContainer.addEventListener("mousemove", handleTimelineUpdate)
 
-//If mouse is pressed down, toggle scrubbing
+//  Se o mouse é pressionado, alterna o gatilho de Scrubbing
 timelineContainer.addEventListener("mousedown", toggleScrubbing)
 
-//Only enter scrubbing when in timeline and clicking down
+// Só acionar o scrubbing quando estiver na timeline e o mouse pressionado
 document.addEventListener("mouseup", e => {
   if (isScrubbing) toggleScrubbing(e)
 })
 
-//If scrubbing starts handleTimelineUpdate
+// Se está em scrubbing, startar handleTimelineUpdate
 document.addEventListener("mousemove", e => {
   if (isScrubbing) handleTimelineUpdate(e)
 })
@@ -74,69 +82,69 @@ let isScrubbing = false
 let wasPaused
 function toggleScrubbing(e) {
   const rect = timelineContainer.getBoundingClientRect()
-  //e.x gives position of X of mouse cursor, relative to timeline.
-  //0 is so cursor doesn't go past limit. 
-  //Rect.width is furthest right position
+  // Entrega a posição x do cursor do mouse, relativo à timeline
+  // o 0 é para que o cursor não ultrapasse o limite.
+  // Rect.width é a posição mais a direita no elemento
   const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width
-  //Determines if left button is being click, if yes, enables scrubbing
+  // Determina se o botão direito está sendo clicado, se sim, permite o evento Scrubbing
   isScrubbing = (e.buttons & 1) === 1
   musicContainer.classList.toggle("scrubbing", isScrubbing)
-  //If scrubbing, pause audio
+  // Se está em scrubbing, pausar o audio
   if (isScrubbing) {
     wasPaused = audio.paused
     audio.pause()
   } else {
-    //Move audio where scrubbing was stopped then play
+    // Mover o audio para onde o scrubbing parou e então dar play
     audio.currentTime = percent * audio.duration
     if (!wasPaused) audio.play()
   }
 
-  //If scrubbing starts, pulls code from handleTimelineUpdate 
+  // Se está em scrubbing, buscar o código de handleTimelineUpdate
   handleTimelineUpdate(e)
 }
 
 function handleTimelineUpdate(e) {
   const rect = timelineContainer.getBoundingClientRect()
-  //e.x gives position of X of mouse cursor, relative to timeline.
-  //0 is so cursor doesn't go past limit. 
-  //Rect.width is furthest right position
+  // Entrega a posição x do cursor do mouse, relativo à timeline
+  // o 0 é para que o cursor não ultrapasse o limite.
+  // Rect.width é a posição mais a direita no elemento
   const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width
-  //Math.floor((percent * audio.duration)) gives value for how far into audio
-  //Determines image according to how they were set up, 10 seconds
+  //Math.floor((porcentagem * duração do audio)) entrega o valor do quão distante está a reprodução do audio
+  //Determina a imagem de acordo com como ela foi configurada, 10 segundos
   timelineContainer.style.setProperty("--preview-position", percent)
 
-  //Scrubbing Settings
+  // Configurações do Scrubbing (vide início do script)
   if (isScrubbing) {
-    //Prevents highlighting page while scrubbing
+    // Previne destaque da imagem enquanto em scrubbing
     e.preventDefault()
     timelineContainer.style.setProperty("--progress-position", percent)
   }
 }
 
-//Current Time
+// Tempo atual
 audio.addEventListener("timeupdate", () => {
   currentTime.textContent = formatDuration(audio.currentTime)
-  //Bar will move with audio progress
+  // Barra se move com a reprodução do audio
   const percent = audio.currentTime / audio.duration
   timelineContainer.style.setProperty("--progress-position", percent)
 })
 
-//Duration Counter
+// Duração
 audio.addEventListener("loadeddata", () => {
   totalTime.textContent = formatDuration(audio.duration)
 })
 
-//Makes time say :04 instead of :4
+// Converte tempo em :04 em vez de :4
 const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
   minimumIntegerDigits: 2,
 })
 
-//Created to display duration time in full instead of seconds
+// Mostra duração completa do audio em vez dos segundos apenas
 function formatDuration(time) {
   const seconds = Math.floor(time % 60)
   const minutes = Math.floor(time / 60) % 60
   const hours = Math.floor(time / 3600)
-  //If no hours display minutes, if so show with minutes
+  // Se não mostrar horas, mostrar somente os minutos, se mostra, exibir horas com os minutos
   if (hours === 0) {
     return `${minutes}:${leadingZeroFormatter.format(seconds)}`
   } else {
